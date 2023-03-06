e4e6581013c7bf20125d0c2f69d67f52bce9412c
FlorianFlouquet <florian.flouquet2@hotmail.com>
2023-03-06 16:21:32 +0100

    flsùfùs

---
+++ back/dist/calcul.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.multiply = void 0;
+const multiply = (a, b) => {
+    return a * b;
+};
+exports.multiply = multiply;
