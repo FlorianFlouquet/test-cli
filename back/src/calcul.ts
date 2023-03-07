b8195b38b419080b2fd8d78a1e42bd1428b47fb6
FlorianFlouquet <florian.flouquet2@hotmail.com>
2023-03-07 12:50:37 +0100

    gregter

--- back/src/calcul.ts
+++ back/src/calcul.ts
@@ -1,10 +1,14 @@
-const multiply = (a: number, b: number, c: number) => {
-    if(b > 0) {
-        return a * b;
-    }
+// const multiply = (a: number, b: number, c: number) => {
+//     if(b > 0) {
+//         return a * b;
+//     }
+// }
+
+const multiply = (a: number, b: number) => {
+    return a * b;
 }
 
-let osef;
+// let osef;
 
 // Example of code that won't pass CI
 // Reason: parseFloat needs a string as argument
