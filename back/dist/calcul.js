d3f68b87fc2b89dfd36e6a228a55b646097e48e2
FlorianFlouquet <64949896+FlorianFlouquet@users.noreply.github.com>
2023-03-07 12:49:04 +0100

    Delete back/dist directory

--- back/dist/calcul.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.multiply = void 0;
-const multiply = (a, b) => {
-    return a * b;
-};
-exports.multiply = multiply;
